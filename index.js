409ae973cdc509f4cdeeb15750bdca90346f944a
Zealtiel <zealtiel@outlook.com>
2023-02-08 22:27:21 -0600

    Fix file serving issue with client side.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 const songlistController = require("./controllers/songlistControllers");
+const path = require("path");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use("/public", express.static(path.join(__dirname, "public")));
 
 app.get("/", songlistController.getFullSongList);
 
@@ -13,10 +14,10 @@ app.get("/favorites", songlistController.getFavorites);
 
 app.get("/search", songlistController.getSearch);
 
-app.get("/:id", songlistController.getSong);
+app.get("/song/:id", songlistController.getSong);
 
-app.put("/:id", songlistController.updateSong);
+app.put("/song/:id", songlistController.updateSong);
 
 app.listen(8080, () => {
-  console.log("You are now listening to port 8080 fm");
+  console.log("Server running at http://localhost:8080");
 });
